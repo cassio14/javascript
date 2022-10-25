eab49c58784bc3d1817887ce1f209b4df9965117
cassio14 <111062550+cassio14@users.noreply.github.com>
2022-10-25 08:42:41 -0300

    ....

---
+++ aula12ex/ex0015/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+    var date = new date()
+    var ano = date.getFullyear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    
+
+}
