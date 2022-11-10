e207a761230b93c269b6554f9fc066da31ec8590
cassio14 <111062550+cassio14@users.noreply.github.com>
2022-11-10 11:03:27 -0300

    ..

---
+++ aula0013/ambiente.js
@@ -0,0 +1,14 @@
+var C = 1
+while (c <= 12) {
+    console.log('oi')
+    c++ 
+}//seguinifica que C recebe C + 1
+
+
+/*
+console.log('oi ')
+console.log('oi ')
+console.log('oi ')
+console.log('oi')
+console.log('oi')
+*/
