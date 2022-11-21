860f2fb635d2a78ae85b61b7bed06b0b7de218fc
cassio14 <111062550+cassio14@users.noreply.github.com>
2022-11-21 08:35:10 -0300

    ..

--- aula0014ex/ex0016/script.js
+++ aula0014ex/ex0016/script.js
@@ -1,11 +1,31 @@
 function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
-    let passo =document.getElementById('txtp')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         Window.alert('[ERRO] Faltando dados!')
     } else {
-        alert('TUDO OK!')
-    }
-} 
+        res.innerHTML = 'contador: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem cresente
+           for(let c = i; c <= f; c += p) {
+               res.innerHTML += `${c} \u{1F449}`
+           } 
+        } else {
+            //Contagem regressiva 
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F3C1}`
+            }        
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
+}
--- aula0014ex/ex0017/script.js
+++ aula0014ex/ex0017/script.js
@@ -1,1 +1,14 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    let n = Number(num.value)
+    } else {
+       let n = Number(num.value)
+       let c = 1
+       while (c <= 10) {
+        let item = document.createElement('option')
+       } 
+    }
+}
