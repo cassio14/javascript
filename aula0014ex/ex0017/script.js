cae20768122fb1a84952f894f9d1d1cd7bfaea68
cassio14 <111062550+cassio14@users.noreply.github.com>
2022-11-22 09:00:24 -0300

    ...

--- aula0014ex/ex0017/script.js
+++ aula0014ex/ex0017/script.js
@@ -3,12 +3,16 @@ function tabuada() {
     let tab = document.getElementById('seltab')
     if (num.value.length == 0) {
         window.alert('Por favor, digite um número!')
-    let n = Number(num.value)
     } else {
        let n = Number(num.value)
        let c = 1
+       tab.innerHTML = ''
        while (c <= 10) {
         let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+        c++
        } 
     }
+
 }
