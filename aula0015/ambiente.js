dad2997358cc1be1f450e567231ac11c37cf5189
cassio14 <111062550+cassio14@users.noreply.github.com>
2022-11-25 11:20:31 -0300

    ..

--- aula0015/ambiente.js
+++ aula0015/ambiente.js
@@ -4,3 +4,9 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro do velor é ${num[1]}`)
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log('O valor não fo encontrado')
+} else {
+    console.log(`O valor  está na posição ${pos}`)
+}
