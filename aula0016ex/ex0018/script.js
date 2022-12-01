bdb05261eae2d0a6a29bbbf75ea280861e63d329
cassio14 <111062550+cassio14@users.noreply.github.com>
2022-12-01 08:28:07 -0300

    ....

---
+++ aula0016ex/ex0018/script.js
@@ -0,0 +1,32 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function  adicionar() {
+    debugger
+    if(isNumero(num.value) && !inlista(num.value, valores)) {
+        valores.push(Number(num.value))   
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+    } else {
+        window.alert('Valor inválido ou ja encontrado na lista.')
+    }
+}
